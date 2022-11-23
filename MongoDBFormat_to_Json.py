2100d8aef1f419fee9ea74e83841aea35fdf250a
Reazor42 <97978472+Reazor42@users.noreply.github.com>
2022-11-23 15:21:11 +0100

    started to implement the rework of the json files

--- MongoDBFormat_to_Json.py
+++ MongoDBFormat_to_Json.py
@@ -1,13 +1,15 @@
 import datetime
 import pymongo
 import json
+import os
+import glob
 
+filename_timestamp = datetime.datetime.now()  # provisional timestamp used for file naming; not final
 
 # ============================================================
 # define link to MongoDB client and name of the database
 client_link = "mongodb+srv://HamidO:123Hamid123@cluster0.f2htr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 database_name = "TierhalterApp"
-
 
 # ============================================================
 # connect to the database
@@ -16,13 +18,9 @@ client = pymongo.MongoClient(client_link)  # access the MongoDB client
 database = client.get_database(database_name)  # access the given database
 
 # access all the different collections
-collections = []
-collections.append(database.get_collection("Krankheiten"))
-collections.append(database.get_collection("Krankheitstyp"))
-collections.append(database.get_collection("Pferdegeschlecht"))
-collections.append(database.get_collection("Pferderasse"))
-collections.append(database.get_collection("Symptom"))
-
+collections = [database.get_collection("Krankheiten"), database.get_collection("Krankheitstyp"),
+               database.get_collection("Pferdegeschlecht"), database.get_collection("Pferderasse"),
+               database.get_collection("Symptom")]
 
 # ============================================================
 # write the data of all collections to a separate json file
@@ -31,15 +29,42 @@ collections.append(database.get_collection("Symptom"))
 print("---fetch relevant data---")
 for col in collections:
     cursor = col.find(
-        {'timestamp':{'$lt':datetime.datetime.now(),
-            '$gt':datetime.datetime.now() - datetime.timedelta(days=20)}})
+        {'timestamp': {'$lt': datetime.datetime.now(),
+                       '$gt': datetime.datetime.now() - datetime.timedelta(days=20)}})
 
     # append all documents to a list
     docs = []
     for doc in list(cursor):
         docs.append(doc)
 
-    # convert the list to json format and write it to a file
+    # create a directory for the current collection
+    os.mkdir(os.path.join(os.getcwd(), col.name))
+
+    # convert the document list to json format and write it to a file
     complete_json = json.dumps(docs, default=str, indent=4)
-    with open('json-data</' + col.name + '.json', 'w') as f:
+    with open(os.path.join(col.name, filename_timestamp.strftime('%Y%m%d_%H%M%S') + '.json'), 'w') as f:
         f.write(complete_json)
+
+# ============================================================
+# rework the created json files
+
+# access the json files in the subdirectories
+json_files = glob.glob('**/*.json')
+
+new_objects = []  # this list will contain the reworked json objects
+# iterate over all json files
+for file in json_files:
+    data = json.loads(open(file).read())
+    # iterate over all objects in the current json file
+    for obj in data:
+        obj['id'] = obj.pop('_id')  # renaming is impossible, so you have to add a new key and copy the value
+        del obj['timestamp']
+        # iterate over all keys in the current object
+        for key in obj:
+            print(obj[key])  # here the key value pairs have to be reworked
+        # append the reworked object
+        new_objects.append(obj)
+
+    # write the reworked objects to the file
+    with open(file, 'w') as f:
+        f.write(json.dumps(new_objects, default=str, indent=4))
