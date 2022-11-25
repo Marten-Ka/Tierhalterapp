876ac677ec8e5725ce050a73a13249dec2876361
Reazor42 <97978472+Reazor42@users.noreply.github.com>
2022-11-25 21:14:40 +0100

    implementation of json rework done (only timestamp format is not final)

--- MongoDBFormat_to_Json.py
+++ MongoDBFormat_to_Json.py
@@ -5,6 +5,7 @@ import pymongo
 import json
 import os
 import glob
+import re
 
 filename_timestamp = datetime.datetime.now()  # provisional timestamp used for file naming; not final
 
@@ -12,6 +13,7 @@ filename_timestamp = datetime.datetime.now()  # provisional timestamp used for f
 # define link to MongoDB client and name of the database
 client_link = "mongodb+srv://HamidO:123Hamid123@cluster0.f2htr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 database_name = "TierhalterApp"
+
 
 # ============================================================
 # connect to the database
@@ -24,15 +26,17 @@ collections = [database.get_collection("Krankheiten"), database.get_collection("
                database.get_collection("Pferdegeschlecht"), database.get_collection("Pferderasse"),
                database.get_collection("Symptom")]
 
+
 # ============================================================
 # write the data of all collections to a separate json file
 
-# iterate over all collections and find all documents of each collection
+# iterate over all collections
 print("---fetch relevant data---")
 for col in collections:
-    cursor = col.find()
-    """ {'timestamp': {'$lt': datetime.datetime.now(),
-                       '$gt': datetime.datetime.now() - datetime.timedelta(days=20)}})"""
+    # find all documents of the last 20 days
+    cursor = col.find(
+        {'timestamp': {'$lt': datetime.datetime.now(),
+                       '$gt': datetime.datetime.now() - datetime.timedelta(days=20)}})
 
     # append all documents to a list
     docs = []
@@ -49,6 +53,7 @@ for col in collections:
     complete_json = json.dumps(docs, default=str, indent=4)
     with open(os.path.join(col.name, filename_timestamp.strftime('%Y%m%d_%H%M%S') + '.json'), 'w') as f:
         f.write(complete_json)
+
 
 # ============================================================
 # rework the created json files
@@ -84,5 +89,20 @@ for file in json_files:
     with open(file, 'w', encoding='cp1252') as f:
         f.write(json.dumps(new_objects, indent=4, ensure_ascii=False))
 
+
 # ============================================================
 # update 'versions.json'
+
+versions = {}
+with open('versions.json', 'r') as v:
+    versions = json.load(v)
+
+for file in json_files:
+    with open(file, 'r') as f:
+        file_data = json.load(f)
+        if file_data:
+            dir_name = re.findall(r'^.*?\\', file)[0][:-1]
+            versions[dir_name] = filename_timestamp
+
+with open('versions.json', 'w') as v:
+    v.write(json.dumps(versions, default=str, indent=4))
